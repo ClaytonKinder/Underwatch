1af5ce9200fd27a7aee402dbb14c37bfef4e8209
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-09-22 16:17:13 -0400

    Starting point.

---
+++ inc/meta.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<title>Underwatch | <?php echo $pageTitle; ?></title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="/media/images/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="/bower_components/skeleton/css/normalize.css" type="text/css">
+	<link rel="stylesheet" href="/bower_components/skeleton/css/skeleton.css" type="text/css">
+	<link rel="stylesheet" href="/styles/styles.css" type="text/css">
+</head>
+<body>
